b76a7c1ecb21943f49389b7ffdf849ae618b3079
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-02-27 14:53:24 +1100

    Added test cases

--- Serializer.UnitTests/JsonSerializerTests.cs
+++ Serializer.UnitTests/JsonSerializerTests.cs
@@ -3,11 +3,66 @@ using NUnit.Framework;
 namespace TISerializer.UnitTests
 {
     [TestFixture]
+    public class ObjectDumperTests
+    {
+        
+    }
+   
+    [TestFixture]
     public class JsonSerializerTests
     {
         [Test]
-        public void UnitAtTest_Conditions_ExpectedBehaviour()
+        public void Deserialize_SerializedString_ReturnsObject()
         {
+            //Deserialize(serialized string)
+
+            //ARRANGE
+
+            //ACT
+            //ASSERT
+            Assert.Fail();
+        }
+        [Test]
+        public void Deserialize_RandmString_Throws()
+        {
+            //ARRANGE
+
+            //ACT
+            //ASSERT
+            Assert.Fail();
+        }
+        [Test]
+        public void Serialize_ValidObject_ReturnsString()
+        {
+            //ARRANGE
+
+            //ACT
+            //ASSERT
+            Assert.Fail();
+        }
+        [Test]
+        public void Serialize_InvalidObject_Throws()
+        {
+            //ARRANGE
+
+            //ACT
+            //ASSERT
+            Assert.Fail();
+        }
+        [Test]
+        public void Deserialize_Type_ReturnsTypeObject()
+        {
+            //ARRANGE
+
+            //ACT
+            //ASSERT
+            Assert.Fail();
+        }
+        [Test]
+        public void Deserialize_InvalidType_Throws()
+        {
+            //different type than the serialized string
+
             //ARRANGE
 
             //ACT
