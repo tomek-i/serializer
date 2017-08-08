c4e8092ccb726101337551745c966ec004ea8efe
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-08-08 14:11:06 +1000

    Add stub for SOAP serializer

---
+++ Serializer.Logic/Serializers/SoapSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TI.Serializer.Logic.Serializers
+{
+    public class SoapSerializer : Serializer
+    {
+        public override T Deserialize<T>(string serializedString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Deserialize(string serializedString, Type type = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Serialize(object anyObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
