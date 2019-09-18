58e51c0939056e09e3cdce530a84c143d7065142
Thomas Iwainski <tomek.iwainski@gmail.com>
2019-09-18 13:20:47 +1000

    added base64 encoder

---
+++ Serializer.Logic/Serializers/Base64Coder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace TI.Serializer.Logic.Serializers
+{
+    public class Base64Coder
+    {
+        public string Decode(string base64text)
+        {
+            var bytes = Convert.FromBase64String(base64text);
+            return new string(Encoding.UTF8.GetChars(bytes));
+        }
+
+
+
+        public string Encode(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
