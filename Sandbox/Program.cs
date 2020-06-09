99be6b686d5d762e1ec68a63ca3c0a63ca689d69
Tomek I <tomek.iwainski@gmail.com>
2020-06-09 13:26:39 +1000

    added sandbox project

---
+++ Sandbox/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TI.Serializer.Logic.Serializers;
+
+namespace Sandbox
+{
+
+
+
+    public class TestClassB
+    {
+        public string Message { get; set; } = "Class B";
+        public object Parent { get; set; }
+
+        public List<int> Numbers { get; set; } = new List<int>() { 1, 2, 3, 4, 5 };
+    }
+
+    public class MainTestClass
+    {
+        public int Number { get; set; } = 99;
+        public bool Bool { get; set; } = true;
+        public string Text { get; set; } = "Main Test Class";
+
+        public object Object { get; set; } = new TestClassB();
+
+      
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var test = new MainTestClass();
+            ((TestClassB)test.Object).Parent = test;
+
+
+            var result = new JsonSerializer().Serialize(test,Newtonsoft.Json.Formatting.Indented,new Newtonsoft.Json.JsonSerializerSettings()
+            {
+                PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects
+            });
+
+            Console.WriteLine("JSON");
+            Console.WriteLine("========");
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+
+        
+
+        }
+    }
+}
